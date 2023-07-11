cb554ee8d511848b989d7b3abedb5dda7737e151
Ömer NAYIR <89137487+omernyr@users.noreply.github.com>
2023-07-11 14:48:25 +0300

    Update APICaller.swift

--- testcall/APICaller.swift
+++ testcall/APICaller.swift
@@ -10,8 +10,8 @@ import Alamofire
 
 func APICaller(_ toNumber: String?) {
     
-    let accountSID = "ACe26f91b64b92d3fc16b157862e313770"
-    let authToken = "6c048fc431c373cc988190d8f2763a7b"
+    let accountSID = "xxx"
+    let authToken = "xxx"
     
     let url = "https://api.twilio.com/2010-04-01/Accounts/\(accountSID)/Calls"
     let parameters = ["From": "+12176287050", "To": "\(toNumber ?? "")", "Url": "https://handler.twilio.com/twiml/EHa3d6efedfa3d45e34cdc2415082ed73e"]
